f47aebc2b4e16844bd67bf5f69d731dbbfaed176
bssthu <bssthu@users.noreply.github.com>
2015-10-15 13:49:21 +0800

    remove strings and comments before diff

--- multi_diff_cpp.py
+++ multi_diff_cpp.py
@@ -6,8 +6,8 @@
 
 import os
 import traceback
-import codecs
 import difflib
+import re
 
 
 # 是否是C++源代码
@@ -17,6 +17,39 @@ def is_source(filename):
         if filename.lower().endswith(ext):
             return True
     return False
+
+
+def format_source(lines):
+    text = '\n'.join(lines)
+    # remove comment and string
+    pattern = re.compile(
+            r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+            re.DOTALL | re.MULTILINE
+    )
+    text = re.sub(pattern, '', text)
+    text = ' '.join(text.split())
+    return text
+
+
+def check_diff(file_list, threshold):
+    bad_list = []
+    for i in range(0, len(file_list)):
+        for j in range(i + 1, len(file_list)):
+            try:
+                fp = open(file_list[i], errors='ignore')
+                text1 = format_source(fp.readlines())
+                fp.close()
+                fp = open(file_list[j], errors='ignore')
+                text2 = format_source(fp.readlines())
+                fp.close()
+
+                simi = difflib.SequenceMatcher(None, text1, text2).ratio()
+                if simi >= threshold:
+                    bad_list.append((simi, file_list[i], file_list[j]))
+            except Exception as e:
+                traceback.print_exc()
+                print(e)
+    return bad_list
 
 
 def main():
@@ -32,23 +65,7 @@ def main():
             if is_source(file_path):
                 file_list.append(file_path)
 
-    bad_list = []
-    for i in range(0, len(file_list)):
-        for j in range(i + 1, len(file_list)):
-            try:
-                fp = open(file_list[i], errors='ignore')
-                lines1 = '\n'.join(fp.readlines())
-                fp.close()
-                fp = open(file_list[j], errors='ignore')
-                lines2 = '\n'.join(fp.readlines())
-                fp.close()
-
-                simi = difflib.SequenceMatcher(None, lines1, lines2).ratio()
-                if simi >= threshold:
-                    bad_list.append((simi, file_list[i], file_list[j]))
-            except Exception as e:
-                traceback.print_exc()
-                print(e)
+    bad_list = check_diff(file_list, threshold)
 
     bad_list.sort(key=lambda x:x[0], reverse=True)
     for pair in bad_list:
